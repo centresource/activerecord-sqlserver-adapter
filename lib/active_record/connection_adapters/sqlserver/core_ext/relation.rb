00bc8d7b14d56de7f84caf8b06f20b3b935a427f
Jeremy Holland <jeremy@jeremypholland.com>
2013-10-23 13:23:33 -0500

    More attempts to get tables_in_string to return correctly

--- lib/active_record/connection_adapters/sqlserver/core_ext/relation.rb
+++ lib/active_record/connection_adapters/sqlserver/core_ext/relation.rb
@@ -1,19 +1,13 @@
-module ActiveRecord
-  module ConnectionAdapters
-    module Sqlserver
-      module CoreExt
-        module Relation
-          
-          private
-          
-          def tables_in_string(string)
-            super - ['__rnt']
-          end
-          
-        end
-      end
+class ::ActiveRecord::Relation
+  private
+  def tables_in_string(string)
+    return [] if string.blank?
+    # always convert table names to downcase as in Oracle quoted table names are in uppercase
+    # ignore raw_sql_ that is used by Oracle adapter as alias for limit/offset subqueries
+    if base_class.table_name =~ /\w+\.\w+/
+      string.scan(/([a-zA-Z_\[][.\w\]\[]+]).?\./).flatten.map { |s| s.downcase.gsub(/[\[\]]/, '') }.uniq - ['raw_sql_', base_class.table_name.split(/\./)[0].to_s.downcase]
+    else
+      string.scan(/([a-zA-Z_][.\w]+).?\./).flatten.map{ |s| s.downcase }.uniq - ['raw_sql_']
     end
   end
 end
-
-ActiveRecord::Relation.send :include, ActiveRecord::ConnectionAdapters::Sqlserver::CoreExt::Relation
